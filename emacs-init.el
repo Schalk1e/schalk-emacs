ccd517f69bb44a23f0e7de7437ce5c9bafdc3976
Schalk1e <sv973005@gmail.com>
2024-02-28 16:59:55 +0200

    First config push. (@Schalk1e)

---
+++ emacs-init.el
@@ -0,0 +1,443 @@
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(eval-when-compile
+  (require 'use-package))
+;; (setq use-package-verbose t)
+
+(use-package async
+  :ensure t
+  :config
+  (defun my/init-hook ()
+    "If the current buffer is 'emacs-init.org' the code-blocks are tangled."
+    (when (equal (buffer-file-name) my-org-file)
+      (async-start
+       `(lambda ()
+          (require 'org)
+          (org-babel-tangle-file ,my-org-file))
+       (lambda (result)
+         (message "Tangled file compiled.")))))
+  (add-hook 'after-save-hook 'my/init-hook))
+
+(add-to-list 'load-path my-elisp-dir)
+
+(prefer-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+
+(use-package ido
+  :config
+  (ido-mode t)
+  (setq ido-everywhere t
+        ;; If the file at point exists, use that
+        ido-use-filename-at-point nil
+        ;; Or if it is an URL...
+        ido-use-url-at-point nil
+        ;; Even if TAB completes uniquely,
+        ;; still wait for RET
+        ido-confirm-unique-completion t
+        ;; If the input does not exist,
+        ;; don't look in unexpected places.
+        ;; I probably want a new file.
+        ido-auto-merge-work-directories-length -1))
+
+(use-package uniquify
+  ;; :ensure t
+  :config
+  (setq uniquify-buffer-name-style 'post-forward
+        uniquify-separator ":"))
+
+;; Syntax highlighting on.
+(global-font-lock-mode 1)
+(defconst font-lock-maximum-decoration t)
+
+;; Show various whitespace.
+(setq whitespace-style '(face empty tabs lines-tail trailing))
+(global-whitespace-mode t)
+(setq-default show-trailing-whitespace t)
+
+;; Enable highlighting when marking a region
+(setq-default transient-mark-mode t)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+
+(setq-default fill-column 79)
+
+(when (eq system-type 'darwin)
+  ;; Fix some keybindings
+  (global-set-key [home] 'move-beginning-of-line)
+  (global-set-key  [end] 'move-end-of-line)
+  ;; Avoid dired/ls errors
+  (setq dired-use-ls-dired nil)
+  ;; Invoke login shells so that .profile or .bash_profile is read
+  (setq shell-command-switch "-lc")
+  ;; Load some pbcopy/pbpaste functions
+  (require 'pbstuff))
+
+(unless (display-graphic-p)
+  (menu-bar-mode -1))
+
+(when (boundp 'aquamacs-version)
+  ;; Make this more Emacsy.
+  (one-buffer-one-frame-mode -1)
+  (tabbar-mode -1)
+
+  ;; Make some keybindings saner.
+  (define-key osx-key-mode-map `[(,osxkeys-command-key w)] nil)
+  (define-key osx-key-mode-map [home] 'move-beginning-of-line)
+  (define-key osx-key-mode-map  [end] 'move-end-of-line)
+  (define-key osx-key-mode-map [A-home] 'beginning-of-buffer)
+  (define-key osx-key-mode-map  [A-end] 'end-of-buffer)
+  (define-key osx-key-mode-map [C-left] 'backward-word)
+  (define-key osx-key-mode-map [C-right] 'forward-word)
+
+  ;; Get rid of the stupid "Mac" modifiers.
+  (setq ns-use-mac-modifier-symbols nil)
+
+  ;; Improve zooming.
+  (require 'zoom-replacement)
+  (define-key osx-key-mode-map `[(,osxkeys-command-key =)] 'zoom-interactive)
+  (define-key osx-key-mode-map `[(,osxkeys-command-key +)] 'zoom-interactive)
+  (define-key osx-key-mode-map `[(,osxkeys-command-key -)] 'zoom-interactive-out))
+
+(unless (boundp 'aquamacs-version)
+  (when (display-graphic-p)
+    ;; Nicer font.
+    (set-face-attribute
+     'default nil
+     :family "Inconsolata" :height 140 :weight 'normal)))
+
+;; Autorevert to make VCS nicer
+(global-auto-revert-mode 1)
+
+;; One space between sentences, please.
+(setq sentence-end-double-space nil)
+
+;; Undo some cruft that may have been done.
+(cua-mode 0)
+(if window-system (tool-bar-mode 0))
+(setq inhibit-startup-screen t)
+
+;; Better behaviour when started with multiple files.
+(setq inhibit-startup-buffer-menu t)
+(setq split-width-threshold 150)
+
+;; Current point in mode bar.
+(line-number-mode t)
+(column-number-mode t)
+
+;; Turn off backups (that's what VCS is for) and move auto-save out the way.
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+(use-package flycheck
+  :ensure t
+  :init
+  (global-flycheck-mode)
+  (setq flycheck-check-syntax-automatically '(mode-enabled save)))
+
+(use-package flyspell
+  :hook
+  ((org-mode markdown-mode) . flyspell-mode)
+  ;; (prog-mode . flyspell-prog-mode)
+  (before-save-hook . flyspell-buffer)
+  :custom
+  (ispell-program-name "aspell")
+  (ispell-extra-args '("--sug-mode=normal" "--master=en_GB-ize-w_accents")))
+
+(setq-default gist-view-gist t)
+
+(use-package org
+  :config
+  (setq org-src-fontify-natively t))
+
+(use-package rainbow-delimiters
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  ;; Apparently this is special?
+  (add-hook 'python-mode-hook 'rainbow-delimiters-mode)
+  :config
+  ;; Set some custom colours based loosely on the zenburn theme.
+  (set-face-attribute 'rainbow-delimiters-depth-1-face nil :foreground "grey55")
+  (set-face-attribute 'rainbow-delimiters-depth-2-face nil :foreground "#f0dfaf")
+  (set-face-attribute 'rainbow-delimiters-depth-3-face nil :foreground "#94bff3")
+  (set-face-attribute 'rainbow-delimiters-depth-4-face nil :foreground "#dca3a3")
+  (set-face-attribute 'rainbow-delimiters-depth-5-face nil :foreground "#8fb28f")
+  (set-face-attribute 'rainbow-delimiters-depth-6-face nil :foreground "#93e0e3")
+  (set-face-attribute 'rainbow-delimiters-depth-7-face nil :foreground "#dfaf8f")
+  (set-face-attribute 'rainbow-delimiters-depth-8-face nil :foreground "#dc8cc3"))
+
+(setq-default c-basic-offset 4)
+(add-hook 'c-mode-common-hook
+          (lambda () (local-set-key "\C-m" 'newline-and-indent)))
+
+(use-package omnisharp-mode
+  :hook csharp-mode
+  :init
+  (setq omnisharp-server-executable-path "/usr/local/bin/omnisharp"))
+
+(use-package clojure-mode
+  :ensure t
+  :init
+  (add-hook 'clojure-mode-hook #'enable-paredit-mode)
+  :config
+  (use-package flycheck-clj-kondo
+    :ensure t))
+
+(use-package cider
+  :ensure t
+  :defer t)
+
+(use-package csv-mode
+  :ensure t)
+
+(use-package dhall-mode
+  :ensure t
+  :config
+  (setq
+   dhall-format-at-save nil))
+
+(use-package dockerfile-mode
+  :ensure t
+  :mode "\\.docker$")
+
+(use-package elixir-mode
+  :mode (("\\.exs?$" . elixir-mode))
+  :config
+  (require 'alchemist)
+  (add-hook 'elixir-mode-hook 'alchemist-mode)
+  (setq alchemist-hooks-compile-on-save t))
+
+(use-package erlang
+  :ensure t)
+
+(use-package fountain-mode
+  :ensure t)
+
+(use-package go-mode
+  :ensure t
+  :hook ((go-mode . lsp)
+         ;; Drop tabs from visible whitespace list
+         (go-mode . (lambda ()
+                      (setq-local whitespace-style
+                                  '(face empty lines-tail trailing))))
+         ;; Let LSP rewrite my file, because Go is too annoying otherwise
+         (before-save . lsp-format-buffer)
+         (before-save . lsp-organize-imports))
+  :config
+  (add-to-list 'exec-path (concat (getenv "GOPATH") "/bin")))
+
+;; (use-package flycheck-gometalinter
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (setq flycheck-gometalinter-fast t)
+;;     (setq flycheck-gometalinter-tests t)
+;;     (setq flycheck-gometalinter-deadline "10s")
+;;     (flycheck-gometalinter-setup)))
+
+;; (use-package flycheck-golangci-lint
+;;   :ensure t
+;;   :hook (go-mode . flycheck-golangci-lint-setup)
+;;   :config
+;;   (setq flycheck-golangci-lint-tests t)
+;;   (setq flycheck-golangci-lint-deadline "5s")
+;;   ;; There's a bug that requires us to stick = on the front.
+;;   (setq flycheck-golangci-lint-config
+;;         (expand-file-name "~/.gostuff/golangci-emacs.yml")))
+
+(use-package graphql-mode
+  :ensure t)
+
+(use-package groovy-mode
+  :mode (("^Jenkinsfile$" . groovy-mode)
+         ("\\.jenkins$" . groovy-mode)
+         ("\\.groovy$" . groovy-mode)))
+
+;; web-mode, please.
+(use-package web-mode
+  :ensure t
+  :mode (("\\.html?$" . web-mode)
+         ("\\.tsx$" . web-mode))
+  :config
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-script-padding 2)
+  ;; Use tidy5 instead of tidy, because we like HTML5.
+  (setq flycheck-html-tidy-executable "tidy5")
+  (add-hook 'web-mode-hook
+        (lambda ()
+          (when (string-equal "tsx" (file-name-extension buffer-file-name))
+            (setup-tide-mode)))))
+
+;; This is like HTML, right?
+(use-package sass-mode
+  :ensure t
+  :mode "\\.scss\\'")
+
+(setq-default js-indent-level 2)
+
+(use-package jq-mode
+  :ensure t
+  :mode (("\\.jq$" . jq-mode)))
+
+(use-package jsonnet-mode
+  :ensure t)
+
+(use-package lsp-mode
+  :ensure t
+  :hook ((python-mode . lsp-deferred)
+         ((rust-mode dhall-mode) . lsp))
+  :config
+  (setq lsp-prefer-flymake nil
+        lsp-enable-snippet nil
+        lsp-headerline-breadcrumb-enable nil)
+  (lsp-register-custom-settings '(("pylsp.plugins.pylsp_mypy.dmypy" nil t)
+                                  ("pylsp.plugins.pylsp_mypy.live_mode" nil t)
+                                  ("pylsp.plugins.pylsp_mypy.report_progress" t t)))
+  :commands (lsp lsp-deferred))
+
+(use-package lsp-ui
+  :ensure t
+  :config
+  (setq lsp-ui-doc-enable nil
+        lsp-ui-flycheck-enable t
+        lsp-ui-flycheck-live-reporting t
+        lsp-ui-sideline-show-hover nil)
+  :commands lsp-ui-mode)
+
+;; (use-package lua-mode
+;;   :ensure t)
+
+(use-package markdown-mode
+  :ensure t)
+
+(use-package tuareg
+  :mode (("\\.ml[ily]?$" . tuareg-mode)
+         ("\\.topml$" . tuareg-mode)
+         ("\\.atd$" . tuareg-mode))
+  :config
+  ;; Undefine this function to stop `<<' triggering camlp4 syntax stuff.
+  (defun tuareg-syntax-propertize (start end))
+
+  (setq opam-share (substring (shell-command-to-string
+                               "opam config var share") 0 -1))
+  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+  (load-file (concat opam-share "/emacs/site-lisp/ocp-indent.el"))
+  (require 'ocp-indent)
+  (require 'merlin)
+
+  (define-key merlin-mode-map
+    (kbd "C-c <up>") 'merlin-type-enclosing-go-up)
+  (define-key merlin-mode-map
+    (kbd "C-c <down>") 'merlin-type-enclosing-go-down)
+
+  (add-hook 'tuareg-mode-hook 'merlin-mode)
+  (add-hook 'tuareg-mode-hook 'ocp-setup-indent)
+  (setq merlin-use-auto-complete-mode 'easy)
+  ;; Use opam switch to lookup ocamlmerlin binary
+  (setq merlin-command 'opam)
+  ;; (setq merlin-error-after-save nil)
+
+  (require 'auto-complete)
+  (setq ac-auto-start nil)
+  (setq ac-candidate-menu-min 0)
+  (setq ac-disable-inline t)
+  ;; (setq ac-auto-show-menu 0.8)
+  ;; (define-key ac-completing-map "\r" nil)
+  (add-hook 'tuareg-mode-hook 'auto-complete-mode))
+
+(use-package octave-mode
+  :mode "\\.m$")
+
+(use-package ox-reveal
+  :ensure t
+  :config
+  (setq org-export-allow-bind-keywords t))
+
+(use-package php-mode
+  :ensure t
+  :custom
+  (php-mode-coding-style 'symfony2))
+
+(use-package powershell
+  :ensure t)
+
+(use-package powershell
+  :ensure t)
+
+(use-package puppet-mode
+  :ensure t)
+
+(use-package python-mode
+  :ensure t
+  :init
+  (setq py-underscore-word-syntax-p nil)
+  :custom
+  ;; This breaks indenting various things.
+  ;; (py-closing-list-dedents-bos t)
+  (py-docstring-syle 'django)
+  (py-docstring-fill-column 79)
+  (py-mark-decorators t)
+  (py-indent-list-style 'one-level-to-beginning-of-statement))
+
+(use-package ruby-mode
+  :mode "\\.rb\\'"
+  :init
+  (setq ruby-use-smie nil)
+  :config
+  (defadvice ruby-indent-line (after unindent-closing-paren activate)
+    (let ((column (current-column))
+          indent offset)
+      (save-excursion
+        (back-to-indentation)
+        (let ((state (syntax-ppss)))
+          (setq offset (- column (current-column)))
+          (when (and (eq (char-after) ?\))
+                     (not (zerop (car state))))
+            (goto-char (cadr state))
+            (setq indent (current-indentation)))))
+      (when indent
+        (indent-line-to indent)
+        (when (> offset 0) (forward-char offset)))))
+  (setq ruby-deep-indent-paren-style nil)
+  (use-package ruby-electric
+    :ensure t)
+  (add-hook 'ruby-mode-hook 'ruby-electric-mode))
+
+;; (add-hook 'rust-mode-hook #'flycheck-rust-setup)
+
+(use-package cargo
+  :ensure t
+  )
+
+(use-package rust-mode
+  :ensure t
+  )
+
+(add-hook 'shell-mode-hook 'emacs-lock-mode)
+
+(use-package terraform-mode
+  :ensure t)
+
+(add-hook 'text-mode-hook
+          (lambda ()
+            (setq-local whitespace-style '(face empty tabs trailing))
+            (turn-on-visual-line-mode)))
+
+(use-package typescript-mode
+  :ensure t)
+
+(setq-default typescript-indent-level 2)
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)))
+
+(use-package yaml-mode
+  :ensure t)
---
+++ init.el
@@ -0,0 +1,124 @@
+;;; init.el --- Where all the magic begins
+
+;;; Commentary:
+;;
+;; This is a thin wrapper around emacs-init.el which is generated from
+;; emacs-init.org.
+
+;;; Code:
+
+(defvar my-init-file (expand-file-name "emacs-init.el" user-emacs-directory)
+  "All configurations stored in this file.")
+
+(defvar my-org-file (expand-file-name "emacs-init.org" user-emacs-directory)
+  "All configurations tangled from this file.")
+
+(defvar my-elisp-dir (expand-file-name "elisp/" user-emacs-directory)
+ "Random elisp that isn't in ELPA or wherever.")
+
+;; (org-babel-load-file
+;;  (expand-file-name "emacs-init.org" user-emacs-directory))
+
+(if (file-exists-p my-init-file)
+    (load-file my-init-file)
+  (progn
+    (org-babel-load-file
+     (expand-file-name "emacs-init.org" user-emacs-directory))))
+
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-yamllintrc ".yamllint.yaml")
+ '(frame-background-mode 'dark)
+ '(package-selected-packages
+   '(org-re-reveal ox-reveal csv-mode powershell php-mode tide typescript-mode fountain-mode rust-mode nim-mode julia-mode flycheck-clj-kondo cider racer graphql-mode groovy-mode async terraform-mode omnisharp flycheck-gometalinter bazel-mode flycheck-elixir alchemist toml-mode python-docstring yaml-mode xterm-color web-mode utop use-package tuareg sonic-pi sass-mode ruby-electric rainbow-identifiers rainbow-delimiters puppet-mode paredit pallet mediawiki markdown-mode haskell-mode go-mode gist flycheck-rust eruby-mode erlang dockerfile-mode color-theme clojure-mode cargo auto-complete))
+ '(python-fill-docstring-style 'symmetric)
+ '(safe-local-variable-values
+   '((org-confirm-babel-evaluate)
+     (lsp-pylsp-plugins-flake8-enabled)
+     (test-case-name . vumi.dispatchers.tests.test_load_balancer)
+     (test-case-name . vumi.dispatchers.tests.test_base)
+     (test-case-name . vumi.dispatchers.tests.test_endpoint_dispatchers)
+     (encoding . utf-8)
+     (test-case-name . vxsandbox.tests.test_sandbox_rlimiter)
+     (test-case-name . vumi.application.tests.test_test_helpers)
+     (test-case-name . vxsandbox.tests.test_worker)
+     (test-case-name . vumi.tests.test_service)
+     (test-case-name . vumi.application.tests.test_base)
+     (test-case-name . vumi.tests.test_config)
+     (test-case-name . vumi.tests.test_worker)
+     (test-case-name . vxsandbox.resources.tests.test_config)
+     (test-case-name . vxsandbox.resources.tests.test_kv)
+     (test-case-name . vxsandbox.resources.tests.test_logging)
+     (test-case-name . vxsandbox.resources.tests.test_outbound)
+     (test-case-name . vxsandbox.resources.tests.test_utils)
+     (test-case-name . vxsandbox.tests.test_protocol)
+     (eval setq lsp-rust-rustflags
+           (concat "-L dependency=" my-project-path "target/sysroot/lib/rustlib/x86_64-blog_os/lib"))
+     (lsp-rust-rustflags . "-L dependency=target/sysroot/lib/rustlib/x86_64-blog_os/lib")
+     (lsp-rust-sysroot . "target/sysroot")
+     (eval setq lsp-rust-rustflags
+           (concat "-L dependency=" my-project-path "target/sysrootlib/rustlib/x86_64-blog_os/lib"))
+     (eval setq lsp-rust-rustflags
+           (concat "-L dependency=" my-project-path "target/sysrootlib/rustlib/x86_64-unknown-none/lib"))
+     (lsp-rust-rustflags . "")
+     (lsp-rust-sysroot . "")
+     (lsp-rust-all-targets)
+     (eval setq lsp-rust-sysroot
+           (concat my-project-path "target/sysroot"))
+     (lsp-rust-sysroot . my-project-path)
+     (eval set
+           (make-local-variable 'my-project-path)
+           (file-name-directory
+            (let
+                ((d
+                  (dir-locals-find-file ".")))
+              (if
+                  (stringp d)
+                  d
+                (car d)))))
+     (lsp-rust-target . x86_64-blog_os.json)
+     (eval add-to-list 'lsp-file-watch-ignored '"[/\\\\]target-cov$")
+     (eval setq flycheck-c/c++-gcc-executable "/usr/local/bin/arm-none-eabi-gcc")
+     (flycheck-disabled-checkers quote
+                                 (c/c++-clang))
+     (flycheck-gcc-definitions quote
+                               ("EFM32HG"))
+     (flycheck-disabled-checkers
+      '(c/c++-clang))
+     (flycheck-gcc-definitions
+      '("EFM32HG"))
+     (setq flycheck-disabled-checkers
+           '(c/c++-clang))
+     (eval setq flycheck-gcc-definitions
+           '("EFM32HG"))
+     (eval setq flycheck-gcc-include-path
+           (list
+            (expand-file-name "~/code/tomu-quickstart/libopencm3/include/")
+            (expand-file-name "~/code/tomu-quickstart/include/")))
+     (eval setq flycheck-disabled-checkers
+           '(c/c++-clang))
+     (eval
+      (setq flycheck-gcc-include-path
+            (list
+             (expand-file-name "~/code/tomu-quickstart/libopencm3/include/")))
+      (setq flycheck-disabled-checkers
+            '(c/c++-clang)))
+     (eval setq flycheck-clang-include-path
+           (list
+            (expand-file-name "~/code/tomu-quickstart/libopencm3/include/")))
+     (eval setq flycheck-gcc-include-path
+           (list
+            (expand-file-name "~/code/tomu-quickstart/libopencm3/include/")))
+     (test-case-name . twisted.test.test_paths)))
+ '(warning-suppress-types '((package reinitialization))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(eruby-comment-face ((t (:inherit font-lock-comment-face :background "color-240"))))
+ '(eruby-standard-face ((t (:background "color-240")))))
